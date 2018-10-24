646fc85b6468c911535df1d80e9776669b2b20ae
Huy Nguyen <xuan-huy.nguyen@ant-tech.eu>
2018-10-24 23:00:20 +0700

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,198 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import { PostcontentService } from '../service/postcontent.service';
+import { DashboardService } from '../service/dashboard.service';
+
+import { LoadingService } from '@swimlane/ngx-ui';
+import { NotificationService } from '@swimlane/ngx-ui';
+const localToken = localStorage.getItem('token')
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  chooseAll;
+  toggleChk;
+  content;
+  arrPages: Observable<any[]>
+  choosePage = false;
+  showProgress = false;
+  arrDayTime = [];
+  arrImages = [];
+  arrPosted = [];
+  scheduled_publish_time: number;
+  isVideo = false;
+  attached_media;
+  percentUploadImage: number;
+  arrImageOnStorage = []
+  constructor(
+    private _db: AngularFireDatabase,
+    public _storage: AngularFireStorage,
+    private _http: Http,
+    private _postcontentservice: PostcontentService,
+    private _dashboardservice: DashboardService,
+    private loadingService: LoadingService,
+    private notificationService: NotificationService
+  ) { }
+
+  ngOnInit() {
+    this.arrPages = this._db.list('postmypage/users/' + localToken + '/pages').valueChanges()
+  }
+  async  upload(image) {
+    let random = Math.floor(Math.random() * 100000)
+    let res = await this._storage.upload('postmypage/' + new Date().getTime() + random, image)
+    let { fullPath } = res.metadata
+    let xxxxx = await this._db.list('postmypage/imageuploaded').push(fullPath)
+    this.arrImageOnStorage.push({
+      key: xxxxx.key,
+      path: fullPath
+    })
+    this.arrImages.push(res.downloadURL)
+  }
+  uploadImages(images) {
+    for (let image of images) {
+      this.upload(image)
+    }
+  }
+  onFileSelected(event) {
+    this.attached_media = [];
+    let images = event.target.files
+    if (images.length) {
+      let isTypeVideo = images[0].type.indexOf('video') == 0
+      if (isTypeVideo) {
+        this.isVideo = true;
+        this.arrImages = [];
+        console.log('video')
+      } else {
+        this.isVideo = false
+        // this.arrImages = []
+      }
+    } else {
+      console.log('Không có file được chọn')
+    }
+    this.uploadImages(images)
+  }
+
+  alert(body) {
+    this.notificationService.create({
+      body: body,
+      styleType: 'success',
+      timeout: 4000,
+      rateLimit: false
+    })
+  }
+  removeImage(img) {
+    let position = this.arrImages.indexOf(img)
+    this.arrImages.splice(position, 1);
+  }
+  resetForm(form) {
+    form.reset()
+    this.arrImages = []
+    this.arrDayTime = []
+    this.isVideo = false;
+    // Loc cac anh trong database uploaded 
+    for (let item_delete of this.arrImageOnStorage) {
+      this._db.list('postmypage/imageuploaded').remove(item_delete.key)
+      this._storage.ref(item_delete.path).delete()
+    }
+    this.arrImageOnStorage = []
+  }
+  onFormSubmit(form) {
+    // Start datetime
+    let formvalue = form.value
+    if (formvalue['pickerAll'] != '|') {
+      console.log('all')
+      let timeAll = this._postcontentservice.getTime(form.value['pickerAll'])
+      this.scheduled_publish_time = timeAll
+    } else {
+      Object.keys(formvalue).map(key => {
+        let dash = 'datetimepicker-'
+        let positionDash = key.indexOf('datetimepicker')
+        if (positionDash != -1 && formvalue[key] != '|') {
+
+          var acc = key.slice(positionDash + dash.length)
+          var huy = this._postcontentservice.getTime(form.value[key])
+          this.arrDayTime[acc] = (huy)
+        }
+      })
+    }
+    // End datetime
+
+
+    let isPageSelected = Object.values(form.value).indexOf(true)
+    if (isPageSelected == -1) {
+      console.log('Chưa chọn page')
+      this.choosePage = true;
+      this.alert('Chọn page muốn đăng')
+    } else {
+      let formvalue = form.value
+      let content = formvalue.content
+      if (content || this.arrImages) {
+        this.loadingService.start()
+        if (this.arrImages.length) {
+          Object.keys(form.value).map(access_token => {
+            if (formvalue[access_token] === true && access_token != 'chooseAll') {
+              if (this.isVideo) {
+                let contentVideo = {
+                  video: this.arrImages[0],
+                  title: formvalue.titleVideo,
+                  description: content
+                }
+                this._postcontentservice.postVideo(this.arrDayTime[access_token], contentVideo, access_token, (err, res) => {
+                  this._dashboardservice.getInfoPage(access_token, (err, info) => {
+                    if (info) {
+                      this.arrPosted.push({ post_id: res.id, page_id: info.id, page_name: info.name })
+                      this.loadingService.complete()
+                      this.resetForm(form)
+                    }
+                  })
+                })
+              } else {
+                let publish_time;
+                this.scheduled_publish_time ? publish_time = this.scheduled_publish_time : publish_time = this.arrDayTime[access_token]
+                this._postcontentservice.postImages(publish_time, content, this.arrImages, access_token, (err, res) => {
+                  this._dashboardservice.getInfoPage(access_token, (err, info) => {
+                    if (info) {
+                      this.arrPosted.push({ post_id: res.id, page_id: info.id, page_name: info.name })
+                      this.loadingService.complete()
+                      this.resetForm(form)
+                    }
+                  })
+                })
+              }
+
+            }
+          })
+        } else {
+          console.log('Post Text')
+          Object.keys(formvalue).map(access_token => {
+            if (formvalue[access_token] === true && access_token != 'chooseAll') {
+              let publish_time;
+              this.scheduled_publish_time ? publish_time = this.scheduled_publish_time : publish_time = this.arrDayTime[access_token]
+
+              this._postcontentservice.postStatus(publish_time, content, access_token, (err, res) => {
+                if (err) {
+                  this.alert('Fail')
+                  this.loadingService.complete()
+                } else {
+                  this._dashboardservice.getInfoPage(access_token, (err, info) => {
+                    if (info) {
+                      this.arrPosted.push({ post_id: res.id, page_id: info.id, page_name: info.name })
+                      this.loadingService.complete()
+                      this.resetForm(form)
+                    }
+                  })
+                }
+              })
+            }
+          })
+        }
+      }
+    }
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,30 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    // apiKey: 'AIzaSyA8Vw2Ll2PhUV1vUDqbCLLCiBCGr4InrJE',
+    // authDomain: 'xuanhuy-db49a.firebaseapp.com',
+    // databaseURL: 'https://xuanhuy-db49a.firebaseio.com',
+    // projectId: 'xuanhuy-db49a',
+    // storageBucket: 'xuanhuy-db49a.appspot.com',
+    // messagingSenderId: '490786365884'
+    apiKey: "AIzaSyBi85QOcNJ2DJmwFbF8y-hZOS25mSbwyG8",
+    authDomain: "postmypage2.firebaseapp.com",
+    databaseURL: "https://postmypage2.firebaseio.com",
+    projectId: "postmypage2",
+    storageBucket: "postmypage2.appspot.com",
+    messagingSenderId: "958796988406"
+    // apiKey: "AIzaSyACsCB_vVrmRsZ_2FTTszezvGgphbwHl1M",
+    // authDomain: "postmypage3.firebaseapp.com",
+    // databaseURL: "https://postmypage3.firebaseio.com",
+    // projectId: "postmypage3",
+    // storageBucket: "postmypage3.appspot.com",
+    // messagingSenderId: "266810161779"
+
+
+  }
+};
