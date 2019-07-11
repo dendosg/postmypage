490a901f745f62e673ccb15621f935c5175d1488
Den Do <dendosg@gmail.com>
2019-07-11 20:33:41 +0700

    improve dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,13 +15,14 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.loadingService.start()
     this._dashboardservice.getMyPages().subscribe(pages => {
+      if (!pages || !pages.length) return this.loadingService.complete();
       pages.forEach(page => {
         const access_token = page['access_token']
         this._dashboardservice.getInfoPage(access_token).subscribe(res => {
+          this.loadingService.complete()
           if(!res) return
           res = res.json()
           this.myPages.push(res)
-          this.loadingService.complete()
         })
       });
     })
