34b9bee0bf2c3992015bebe1717415a55fa16814
xuanhuy <huynguyen@asimtelecom.vn>
2021-07-12 21:12:09 +0700

    fix get accounts

--- src/app/service/pages.service.ts
+++ src/app/service/pages.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
-import { get } from "lodash";
+import {Injectable} from "@angular/core";
+import {Headers, Http} from "@angular/http";
+import {get} from "lodash";
+import {HttpHeaders} from "@angular/common/http";
+import {RequestOptionsArgs} from "@angular/http/src/interfaces";
 
 @Injectable()
 export class PagesService {
   arrPages = [];
   public baseUrl =
-    "https://cors-anywhere.herokuapp.com/https://graph.facebook.com/me/accounts?access_token=";
-  constructor(private _http: Http) {}
+    "https://graph.facebook.com/me/accounts?limit=50&access_token=";
+
+  constructor(private _http: Http) {
+  }
+
   getExtendedToken(access_token: string) {
     const ngaoDuKyClientId = "249127368869539";
     const ngaoDuKySecret = "af95ccd133ddea7c52872ddac7c4994f";
@@ -26,7 +31,7 @@ export class PagesService {
   // Return all page
   public getAllPages(access_token) {
     let allPages = [];
-    return new Promise((resolve, recject)=>{
+    return new Promise((resolve, recject) => {
       this.getPages(this.baseUrl + access_token, pages => {
         if (pages.length) {
           allPages = allPages.concat(pages);
@@ -36,15 +41,29 @@ export class PagesService {
       });
     })
   }
+
+  public sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   public getPages(endpoint, callback): Promise<any> {
+    const httpOptions = {
+      headers: new Headers({
+        'origin': null
+      })
+    };
+
     return this._http
-      .get(endpoint)
+      .get("https://cors-anywhere.herokuapp.com/" + endpoint, httpOptions)
       .toPromise()
       .then(res => res.json())
-      .then(res => {
+      .then(async res => {
         const pages = get(res, "data");
         callback(pages);
         const newEndPoint = get(res, "paging.next");
+        console.log('Sleep')
+        await this.sleep(1000)
+        console.log('Continue', newEndPoint)
         if (newEndPoint) return this.getPages(newEndPoint, callback);
         callback([]);
       });
